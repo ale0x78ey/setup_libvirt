c57fb34c396ec56241711feee1974535ffae0bfa
Alexey Ulyanov <ale0x78ey@yandex.ru>
2021-08-02 14:40:38 +0000

    local env

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" > /dev/null && pwd)"
-pip install -r ${SCRIPT_DIR}/requirements.txt
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" > /dev/null && pwd)" && \
+  pip install -r "${SCRIPT_DIR}/requirements.txt"
 
 ansible-galaxy collection install amazon.aws
