5ca814591498f0799a13c880e96292ce7cccc268
Alexey Ulyanov <ale0x78ey@yandex.ru>
2021-08-09 19:42:08 +0000

    define and start VMs

--- setup.sh
+++ setup.sh
@@ -4,3 +4,4 @@ SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" > /dev/null && pwd)" && \
   pip install -r "${SCRIPT_DIR}/requirements.txt"
 
 ansible-galaxy collection install amazon.aws
+ansible-galaxy collection install community.libvirt
